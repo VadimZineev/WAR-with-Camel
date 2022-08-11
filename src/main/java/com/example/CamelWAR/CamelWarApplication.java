27978b3324a7a59901fb424697e62ff02c4ba680
VadimZineev <vadimz026@gmail.com>
2022-08-11 17:20:22 +0300

    First commit

---
+++ src/main/java/com/example/CamelWAR/CamelWarApplication.java
@@ -0,0 +1,13 @@
+package com.example.CamelWAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelWarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CamelWAR/CamelWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CamelWAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
